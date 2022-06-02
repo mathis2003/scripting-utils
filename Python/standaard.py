772d9fe9c3548423b1cd7b375e463b3ba892f71c
reyniersbram <55666730+reyniersbram@users.noreply.github.com>
2022-06-02 20:42:27 +0200

    Update standaard.py

--- Python/standaard.py
+++ Python/standaard.py
@@ -46,3 +46,7 @@ filter_obj = filter(lambda x: x % 2 == 0, [1, 2, 3, 4, 5, 5, 6, 7, 8])
 # merk op dat filter_obj GEEN array is of wat dan ook, maar er kan wel over gelooped worden
 for elem in filter_obj:
     print(elem)
+# beter zou zijn om het met een comprehension te doen, tis is sneller en meer pythonic:
+filtered_obj = (x % 2 == 0 for x in range(1, 8))
+# bovenstaand geeft geen lijst terug maar een generator-object, om een lijst te krijgen gebruik je vierkante haakjes:
+filtered_list = [x % 2 == 0 for x in range(1, 8)]
